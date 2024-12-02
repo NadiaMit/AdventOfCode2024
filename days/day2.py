4d6b5ba2a2188c0684b4566ff8a4188d4e7d94ff
Nadia Mitterer <nadia.mitterer@yahoo.it>
2024-12-02 13:24:48 +0100

    finished part 2, improved day

--- days/day2.py
+++ days/day2.py
@@ -12,31 +12,34 @@ input = helpers.read_input(day, test=isTest)
 # code for both parts
 reports = [[int(i) for i in line.split()] for line in input]
 
+def check_is_safe(report):
+  diffs = [report[i] - report[i+1] for i in range(len(report)-1)]
+  # check if all the diffs are between 1 and 3
+  amount_safe = all([0 < abs(diff) < 4 for diff in diffs])
+  # check the signs are all the same
+  sign_safe = all([math.copysign(1, diff) == math.copysign(1, diffs[0]) for diff in diffs])
+  
+  return amount_safe and sign_safe
+
 # part 1
 result_part_1 = 0
+result_part_2 = 0
 for report in reports:
-  sign = None
-  safe = True
-  for i in range(len(report)-1):
-    diff = report[i] - report[i+1]
-    
-    if sign is None:
-      sign = math.copysign(1, diff)
-    elif sign != math.copysign(1, diff):
-      safe = False
-      break
-    
-    if abs(diff) < 1 or abs(diff) > 3:
-      safe = False
-      break
+  safe = check_is_safe(report)
 
   if safe:
     result_part_1 += 1
+    result_part_2 += 1
+  else:
+    for i in range(len(report)):
+      copy = report.copy()
+      copy.pop(i)
+      if check_is_safe(copy):
+        result_part_2 += 1
+        break
 
-# part 2
-result_part_2 =  0
 
 # print the results
 print(f"--- Day {day}: ---")
-print(f"Part 1: {result_part_1}")
-print(f"Part 2: {result_part_2}")
+print(f"Part 1: {result_part_1}") #670
+print(f"Part 2: {result_part_2}") #700
