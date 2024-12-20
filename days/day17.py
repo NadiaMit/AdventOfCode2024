8979c93d9e684413749b094255ad8650230a8da8
NadiaMit <nadia.mitterer@yahoo.it>
2024-12-20 16:17:52 +0100

    started day 17

--- days/day17.py
+++ days/day17.py
@@ -1,6 +1,7 @@
 import sys
 import time
 import re
+import math
 
 sys.path.insert(1, sys.path[0].replace("days", "helpers"))
 import helpers as helpers
@@ -30,31 +31,63 @@ program_string = program_string.split(",")
 for i in range(0, len(program_string), 2):
   program.append((int(program_string[i]), int(program_string[i+1])))
 
+output = []
 halt = False
-while not halt:
+while not halt or pinter < len(program):
   command, value = program[pointer]
+
+  # get new value 
+  use_value = value
+  if value == 4:
+    use_value = registers["A"]
+  elif value == 5:
+    use_value = registers["B"]
+  elif value == 6:
+    use_value = registers["C"]
+  elif value == 7:
+    print("ERROR SHOULD NOT BE HERE")
+    break
   
   if command == 0:
-    pass
+    a = registers["A"]
+    registers["A"] = math.trunc(a/2**use_value)
+    pointer += 1
+    
   elif command == 1:
-    pass
+    b = registers["B"]
+    # bitwise XOR with value
+    registers["B"] = b ^ value
+    pointer += 1
+    
   elif command == 2:
-    pass
+    registers["B"] = use_value % 8
+    pointer += 1
+    
   elif command == 3:
     pass
   elif command == 4:
-    pass
+    b = registers["B"]
+    c = registers["C"]
+    registers["B"] = b ^ c
+    pointer += 1
+    
   elif command == 5:
-    pass
+    output.append(use_value%8)
+    pointer += 1
+    
   elif command == 6:
-    pass
+    a = registers["A"]
+    registers["B"] = math.trunc(a/2**use_value)
+    pointer += 1
+    
   elif command == 7:
-    pass
-  
-  halt = True
+    a = registers["A"]
+    registers["C"] = math.trunc(a/2**use_value)
+    pointer += 1
+
 
 # part 1
-result_part_1 = 0
+result_part_1 = str.join(",", [str(x) for x in output])
 
 # part 2
 result_part_2 =  0
