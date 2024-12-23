656b569077171ed3bbfe23df7b99c538d9bbfe90
Nadia Mitterer <nadia.mitterer@yahoo.it>
2024-12-23 22:45:02 +0100

    day 23 part 2

--- days/day23.py
+++ days/day23.py
@@ -19,22 +19,38 @@ for line in input:
   graph.setdefault(com[0], []).append(com[1])
   graph.setdefault(com[1], []).append(com[0])
 
-inter_connected = set()
+connections = set()
 for node, neighbors in graph.items():
   for neighbor in neighbors:
     common_neighbors = set(graph[node]).intersection(graph[neighbor])
     for shared_node in common_neighbors:
-      if node.startswith("t") or neighbor.startswith("t") or shared_node.startswith("t"):
-        inter_connected.add(tuple(sorted([node, neighbor, shared_node])))
+      connections.add(tuple(sorted([node, neighbor, shared_node])))
 
 # part 1
-result_part_1 = len(inter_connected)
+result_part_1 = 0
+for connection in connections:
+  if any([x.startswith("t") for x in connection]):
+    result_part_1 += 1
 
 # part 2
-result_part_2 =  0
+biggest_connections = connections.copy()
+
+while True:
+  new_connections = set()
+  for clique in biggest_connections:
+    for node in graph:
+      if node not in clique and all(node in graph[neighbor] for neighbor in clique):
+        new_clique = tuple(sorted(list(clique) + [node]))
+        new_connections.add(new_clique)
+  
+  if len(new_connections) == 0:
+    break
+  biggest_connections = new_connections
+  
+result_part_2 = (',').join(max(biggest_connections, key=lambda x: len(x)))
 
 # print the results
 print(f"--- Day {day}: ---")
 print(f"Part 1: {result_part_1}") #1075
-print(f"Part 2: {result_part_2}")
+print(f"Part 2: {result_part_2}") #az,cg,ei,hz,jc,km,kt,mv,sv,sx,wc,wq,xy
 print(f"Duration: {time.time() - start_time} seconds")
