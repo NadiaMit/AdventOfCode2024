b741a286a70832bb3b30d60428dbe490a73368c2
Nadia Mitterer <nadia.mitterer@yahoo.it>
2024-12-23 16:19:32 +0100

    day 22 part 2 should work but for some reason is not

--- days/day22.py
+++ days/day22.py
@@ -21,16 +21,30 @@ def evolve(secret):
   secret ^= (secret * 2048) %16777216
   return secret
 
-# part 1
 result_part_1 = 0
+
+change_sequences = {}
 for secret in initial_numbers:
-  result_part_1 += [secret := evolve(secret) for _ in range(2000)][-1]
+  secrets = [secret] + [secret := evolve(secret) for _ in range(2000)]
+  prices = [price % 10 for price in secrets]
+  changes = [prices[i] - prices[i-1] for i in range(1,len(prices))]
+  
+  # part 1
+  result_part_1 += secrets[-1]
+  
+  # part 2
+  seen = set()
+  for i in range(len(changes)-4):
+    sequence = tuple(changes[i:i+4])
+    if sequence not in seen:
+      seen.add(sequence)
+      change_sequences[sequence] = change_sequences.get(sequence, 0) + prices[i+4]
 
 # part 2
-result_part_2 =  0
+result_part_2 = max(change_sequences.values())
 
 # print the results
 print(f"--- Day {day}: ---")
 print(f"Part 1: {result_part_1}") #20071921341
-print(f"Part 2: {result_part_2}")
+print(f"Part 2: {result_part_2}") # not 2235
 print(f"Duration: {time.time() - start_time} seconds")
